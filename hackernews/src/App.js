629af40719474611cfd3960e7197f2d3e9e543ca
shyang <shyang@openbase.co.kr>
2018-08-27 17:19:24 +0900

    user 추가

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -3,10 +3,15 @@ import './App.css';
 
 class App extends Component {
   render() {
-    var helloworld = "리엑트에 오신 여러분들 환영합니다";
+    const helloworld = "리엑트에 오신 여러분들 환영합니다";
+    const user = {
+      firstName: 'sh',
+      lastName: 'Yang'
+    };
+
     return (
       <div className = "App">
-        <h2>{helloworld}</h2>
+        <h2>{user.firstName}{user.lastName}님, {helloworld}</h2>
       </div>      
     );
   }
